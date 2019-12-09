9d64ed6e104c75cfe2de06763c3767088f54a95c
alexrobaina <alexrobainaph@gmail.com>
2019-12-09 17:48:00 -0300

    first commit

---
+++ src/pages/prueba.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import header from "../components/header"
+
+class Prueba extends Component {
+  render() {
+    return (
+      <div>
+        <header />
+        soy una prueba
+      </div>
+    )
+  }
+}
+
+export default Prueba
